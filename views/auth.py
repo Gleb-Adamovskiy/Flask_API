14e07c27266fe8317e853c5a2991fb8cac745468
Gleb-Adamovskiy <54521870+Gleb-Adamovskiy@users.noreply.github.com>
2022-05-29 09:01:23 +0700

    Update auth.py

--- views/auth.py
+++ views/auth.py
@@ -1,5 +1,5 @@
 import json
-from flask import request
+from flask import request, jsonify
 from flask_restx import Resource, Namespace
 from implemented import auth_service
 
@@ -12,7 +12,7 @@ class AuthView(Resource):
         user = auth_service.create_new_user(email=req_json.get('email'),
                                             password=req_json.get('password'))
         token = auth_service.encode_auth_token(user.id)
-        return json.dump({"access_token":token.decode()}), 201
+        return jsonify({"access_token":token.decode()}), 201
 
 @auth_ns.route('/login')
 class AuthView(Resource):
@@ -21,7 +21,7 @@ class AuthView(Resource):
         user = auth_service.get_by_email(email=req_json.get('email'))
         token = auth_service.decode_auth_token(req_json.get("access_token").encode())
         result = int(token) == int(user.id)
-        return json.dump({"result":result}), 200
+        return jsonify({"result":result}), 200
 
     def put(self):
         req_json = request.json
@@ -29,4 +29,4 @@ class AuthView(Resource):
         refresh_token = auth_service.decode_auth_token(req_json.get("refresh_token").encode())
         if token == refresh_token:
             token = auth_service.encode_auth_token(token, days=10)
-            return json.dumps(token), 201
+            return jsonify(token), 201
